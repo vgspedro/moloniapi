368d4a7305f115d03a7666a114bd02304a43127b
Pedro Viegas <pedro.viegas@yellowfishtransfers.com>
2020-07-22 13:19:45 +0100

    classes added

--- src/Authentication.php
+++ src/Authentication.php
@@ -2,7 +2,7 @@
 
 namespace VgsPedro\MoloniApi;
 
-class Moloni
+class Authentication
 {
 
 	/**
@@ -15,6 +15,148 @@ class Moloni
 	{
 		$url = $c['url'].'/grant/?grant_type=password&client_id='.$c['client_id'].'&client_secret='.$c['client_secret'].'&username='.$c['username'].'&password='.$c['password'];
 		return $this->curl($url);
+	}
+
+	/**
+	* Get all Customers of the Company 
+	* @return json
+	* https://www.moloni.pt/dev/index.php?action=getApiDocDetail&id=306
+	**/
+	public function getCustomerCount(){
+	
+		$token = $this->login();
+
+		if($token['status'] == 0)
+			return $token;
+
+		$url = $this->url."/customers/count/?access_token=".$token['data']->access_token;
+		
+		return $this->curl($url);
+	}
+
+	/**
+	* Create a new Customer in the Company 
+	* @param array $a Customer information 
+	* @return json 
+	* https://www.moloni.pt/dev/index.php?action=getApiDocDetail&id=204
+	**/
+	public function setCustomer(array $a)
+	{
+		$token = $this->login();
+
+		if($token['status'] == 0)
+			return $token;
+
+		$url = $this->url."/customers/insert/?access_token=".$token['data']->access_token;
+
+		$response = $this->curl($url, [
+			'company_id' => $this->company_id,
+			'vat' => $a['vat'], //NIPC
+			'number' => $a['cid'], //SNC
+			'language_id' => $a['language_id'],
+			'name' => $a['name'],
+			'address' => $a['address'],
+			'city' => $a['city'],
+			'zip_code' => $a['zip_code'], //if country_id = 1 then 0000-000
+			'discount' => $a['discount'],
+			'credit_limit' => $a['credit_limit'],
+			'payment_day' => $a['payment_day'],
+			'country_id' => $a['country_fiscal_id'],
+			'maturity_date_id' => $a['maturity_date_id'], 
+			'qty_copies_document' => $a['qty_copies_document'],
+			'payment_method_id' => $a['payment_method_id'],
+			'copies'=> [
+				'document_type_id' => $a['copies']['document_type_id'],
+				'copies' => $a['copies']['copies'],
+			],
+			'delivery_method_id' => $a['delivery_method_id'],
+			'salesman_id' => $a['salesman_id']
+		]);
+
+		return $response;
+	}
+
+	/**
+	* Get Customer by Id
+	* @param int $id Customer Id // $this->getCustomerByVat(string $vat)
+	* @return json
+	* https://www.moloni.pt/dev/index.php?action=getApiDocDetail&id=199 
+	**/
+	public function getCustomerById(int $id)
+	{
+		$token = $this->login();
+
+		if($token['status'] == 0)
+			return $token;
+		
+		$url = $this->url."/customers/getOne/?access_token=".$token['data']->access_token;
+		$response = $this->curl($url, ['company_id' => $this->company_id, 'customer_id' => $id]);
+
+		return $response;
+	}
+
+	/**
+	* Get Customer by Vat
+	* @param string $vat Customer Vat // '123456789'
+	* @return json 
+	* https://www.moloni.pt/dev/index.php?action=getApiDocDetail&id=201
+	**/
+	public function getCustomerByVat(string $vat)
+	{
+
+		$token = $this->login();
+
+		if($token['status'] == 0)
+			return $token;
+
+		$url = $this->url."/customers/getByVat/?access_token=".$token['data']->access_token;
+		$response = $this->curl($url, ['company_id' => $this->company_id, 'vat' => $vat]);
+
+		return $response;
+	}
+
+	/**
+	* Update Customer by Id
+	* @param array $a Customer information //test set customer_id =  30259661
+	* @return json 
+	* https://www.moloni.pt/dev/index.php?action=getApiDocDetail&id=205
+	**/
+	public function updateCustomerById(array $a)
+	{
+
+		$token = $this->login();
+
+		if($token['status'] == 0)
+			return $token;
+
+		$url = $this->url."/customers/update/?access_token=".$token['data']->access_token;
+
+		$response = $this->curl($url, [
+			'company_id' => $this->company_id,
+			'customer_id' => 30259661,
+			'vat' => $a['vat'], //NIPC
+			'number' => $a['cid'], //SNC
+			'language_id' => $a['language_id'], //1=>PT, 2=>EN, 3=>ES
+			'name' => $a['name'],
+			'address' => $a['address'],
+			'city' => $a['city'],
+			'zip_code' => $a['zip_code'], //if country_id = 1 then 0000-000
+			'discount' => $a['discount'],
+			'credit_limit' => $a['credit_limit'],
+			'payment_day' => $a['payment_day'],
+			'country_id' => $a['country_fiscal_id'],
+			'maturity_date_id' => $a['maturity_date_id'], //18253 PP || 18252 30d || 18254 60d || 18255 90d
+			'qty_copies_document' => $a['qty_copies_document'],
+			'payment_method_id' => $a['payment_method_id'], //17236 cash || xxxx paypal
+			'copies'=> [
+				'document_type_id' => $a['copies']['document_type_id'],
+				'copies' => $a['copies']['copies'],
+			],
+			'delivery_method_id' => $a['delivery_method_id'],
+			'salesman_id' => $a['salesman_id']
+		]);
+
+		return $response;
 	}
 
 	/**
--- src/Classes/Taxes.php
+++ src/Classes/Taxes.php
@@ -1,60 +1,114 @@
 <?php
+
 namespace VgsPedro\MoloniApi\Classes;
 
+use VgsPedro\MoloniApi\Authentication;
+
 /**
- * A class which creates get taxes request
+ * A class which CRUD the Taxes requests
  */
-class Taxes {
 
-	/** @const string Uri to required api */
-	//const URI = "/nativecheckout/v2/installments";
-	const URI = "/taxes/getAll/";
-
-	/** @const string Request method */
-	const METHOD = "POST";
-
-	/** @var int taxId*/
-	private $taxId;
+class Taxes extends Authentication{
 
 	/**
-	 * Sets Tax Id
-	 *
-	 * @param $int $taxId Tax Id
-	 *
-	 * @return \Transaction\Taxesn
-	 */
-	public function setTaxId($taxId) {
+	* List Taxes of Company 
+	* @return json 
+	* https://www.moloni.pt/dev/index.php?action=getApiDocDetail&id=262
+	**/
+	public function getTaxes(array $c)
+	{
 
-		if (!is_string($taxId) && !is_int($taxId)) {
+		$token = $this->login($c);
 
-			return false;
-		}
+		if($token['status'] == 0)
+			return $token;
 
-		$this->taxId = (int) $taxId;
-
-		return $this;
+		$url = $c['url']."/taxes/getAll/?access_token=".$token['data']->access_token;
+		$response = $this->curl($url, ['company_id' => $c['company_id']]);
+		return $response;
 	}
 
 	/**
-	 * Gets tax id
-	 *
-	 * @return int
-	 */
-	public function geTaxId() {
+	* Create Tax in the Company 
+	* @param array $t Tax // This->getTaxes($c) required
+	* @return json 
+	* https://www.moloni.pt/dev/index.php?action=getApiDocDetail&id=263
+	**/
+	public function setTax(array $c, array $t = [])
+	{
+		$token = $this->login($c);
 
-		return $this->taxId;
+		if($token['status'] == 0)
+			return $token;
+
+		$url = $c['url']."/taxes/insert/?access_token=".$token['data']->access_token;
+
+		$response = $this->curl($url, [
+			'company_id' => $c['company_id'],
+			'name' => $t['name'], 
+			'value' => $t['value'],
+			'type' => $t['type'],
+			'saft_type' => $t['saft_type'],
+			'vat_type' => $t['vat_type'],
+			'stamp_tax' => $t['stamp_tax'],
+			'exemption_reason' => $t['exemption_reason'],
+			'fiscal_zone' => $t['fiscal_zone'], //$moloni->getFiscalZones($id)
+			'active_by_default' => $t['active_by_default']
+		]);
+
+		return $response;
 	}
 
 	/**
-	 * Sends request to api
-	 *
-	 * @return stdClass
-	 */
-	public function send() {
+	* Update Tax by Id
+	* @param array $t Tax information // $this->getTaxes($c) required
+	* @return json
+	* https://www.moloni.pt/dev/index.php?action=getApiDocDetail&id=264
+	**/
+	public function updateTax(array $t = [])
+	{
+		$token = $this->login($c);
 
-		$this->setHeaders(["company_id" => $this->getCompanyId()]);
-		$this->setExpectedResult(["maxInstallments" => "Max installments"]);
+		if($token['status'] == 0)
+			return $token;
 
-		return parent::send();
+		$url = $c['url']."/taxes/update/?access_token=".$token['data']->access_token;
+
+		$response = $this->curl($url, [
+			'company_id' => $c['company_id'],
+			'tax_id' => 1999147,
+			'name' => $t['name'], 
+			'value' => $t['value'],
+			'type' => $t['type'],
+			'saft_type' => $t['saft_type'],
+			'vat_type' => $t['vat_type'],
+			'stamp_tax' => $t['stamp_tax'],
+			'exemption_reason' => $t['exemption_reason'],
+			'fiscal_zone' => $t['fiscal_zone'], //$this->getFiscalZones($id)
+			'active_by_default' => $t['active_by_default']
+		]);
+
+		return $response;
 	}
+
+	/**
+	* Delete a Tax from the Company 
+	* @param int $tax_id // $this->getTaxes($c) required
+	* @return json
+	* https://www.moloni.pt/dev/index.php?action=getApiDocDetail&id=265
+	**/
+	public function deleteTax(int $tax_id = 0)
+	{
+
+		$token = $this->login($c);
+
+		if($token['status'] == 0)
+			return $token;
+
+		$url = $c['url']."/taxes/delete/?access_token=".$token['data']->access_token;
+		$response = $this->curl($url, ['company_id' => $c['company_id'], 'tax_id' => $tax_id]);
+		return $response;
+	}
+
+
 }
